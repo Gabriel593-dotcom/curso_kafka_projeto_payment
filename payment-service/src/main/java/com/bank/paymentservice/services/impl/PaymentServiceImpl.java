b28511e62fa3f2c5a47eaf15a332f72857b620ac
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2023-07-20 17:01:52 -0300

    criando um kafka template com serialização de objetos json

--- payment-service/src/main/java/com/bank/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/bank/paymentservice/services/impl/PaymentServiceImpl.java
@@ -1,18 +1,28 @@
 package com.bank.paymentservice.services.impl;
 
-import org.springframework.stereotype.Service;
-
 import com.bank.paymentservice.models.dtos.Payment;
 import com.bank.paymentservice.services.PaymentService;
-
+import java.io.Serializable;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Log4j2
 @Service
-public class PaymentServiceImpl implements PaymentService{
+public class PaymentServiceImpl implements PaymentService {
 
-    @Override
-    public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment.toString());
-    }
+  private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+  @SneakyThrows
+  @Override
+  public void sendPayment(Payment payment) {
+    log.info("Recebi o pagamento {}", payment.toString());
+    Thread.sleep(1000);
+
+    log.info("Enviando pagamento...");
+    kafkaTemplate.send("payment-topic", payment);
+  }
 }
